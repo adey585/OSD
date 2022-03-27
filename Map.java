479a2d2a633925b9889d798e5e9e75cf56d69bd7
adey585 <43513029+adey585@users.noreply.github.com>
2022-03-27 15:22:55 -0400

    Add files via upload

---
+++ Map.java
@@ -0,0 +1,20 @@
+package hwweek6;
+import java.util.*;
+
+public class Map {
+    private List<Road> roads;
+
+    public Map() {
+        roads = new ArrayList<Road>();
+    }
+
+    public void AddRoad(Road road) {
+        roads.add(road);
+    }
+
+    public void Print(IPrintDriver pd, Object o) {
+        for(Road road : roads) {
+            road.Print(pd, o);
+        }
+    }
+}
